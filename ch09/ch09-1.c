26814869d1d06dcb0e479ee4ac26f934f5673439
김수경 <peshello014@gmail.com>
2025-09-24 18:34:01 +0900

    수정.

--- ch09/ch09-1.c
+++ ch09/ch09-1.c
@@ -11,96 +11,103 @@
 #include <stdlib.h>
 
 typedef struct student {
-	char name[24];
-	char id[16];
-	char major[32];
-	int year;
+    char name[24];
+    char id[16];
+    char major[32];
+    int year;
 }Student;
 
 void run();
-void inputStudent(char *str);
-void tokStudent(char *str, Student *data, int *idx);
-void printStudent(Student *data, int *idx);
+void inputStudent(char* str);
+void tokStudent(char* str, Student* data, int* idx);
+void printStudent(Student* data, int* idx);
 
 int main()
 {
-	run();
+    run();
 
-	return 0;
+    return 0;
 }
 
 void run()
 {
-	Student data[128] = { 0 };
-	char str[128];
-	int idx = 0;
+    Student data[128] = { 0 };
+    char str[128];
+    int idx = 0;
 
-	do {
-		inputStudent(str);
-		tokStudent(str, data, &idx);
-		printStudent(data, &idx);
-	} while (strcmp(str, "exit") != 0);
+    do {
+        inputStudent(str);
 
-	return;
+        if (strcmp(str, "exit") == 0)
+        {
+            break;
+        }
+
+        tokStudent(str, data, &idx);
+        printStudent(data, &idx);
+    } while (1);
+
+    return;
 }
 
-void inputStudent(char *str)
+void inputStudent(char* str)
 {
-	printf("이름 | 학번 | 학과 | 학년(정수) 입력(종료하려면 exit): ");
-	gets_s(str, 128);
-	str[strcspn(str, "\n")] = 0;
+    printf("이름|학번|학과|학년(정수) 입력(종료하려면 exit): ");
+    gets_s(str, 128);
+    str[strcspn(str, "\n")] = 0;
 
-	return;
+    return;
 }
 
-void tokStudent(char *str, Student *data, int *idx)
+void tokStudent(char* str, Student* data, int* idx)
 {
-	char* token = 0;
-	char* context = NULL;
+    char* token = 0;
+    char* context = NULL;
 
-	token = strtok_s(str, "|", &context);
+    token = strtok_s(str, "|", &context);
 
-	for (int i = 0; i <= 3; i++)
-	{
-		if (token != NULL)
-		{
-			if (i == 0)
-			{
-				strcpy(data[*idx].name, token);
-				token = strtok_s(NULL, "|", &context);
-			}
-			else if (i == 1)
-			{
-				strcpy(data[*idx].id, token);
-				token = strtok_s(NULL, "|", &context);
-			}
-			else if (i == 2)
-			{
-				strcpy(data[*idx].major, token);
-				token = strtok_s(NULL, "|", &context);
-			}
-			else
-			{
-				data[*idx].year = atoi(token);
-				token = NULL;
-			}
-		}
-	}
-	(*idx)++;
+    for (int i = 0; i <= 3; i++)
+    {
+        if (token != NULL)
+        {
+            if (i == 0)
+            {
+                strcpy(data[*idx].name, token);
+                token = strtok_s(NULL, "|", &context);
+            }
+            else if (i == 1)
+            {
+                strcpy(data[*idx].id, token);
+                token = strtok_s(NULL, "|", &context);
+            }
+            else if (i == 2)
+            {
+                strcpy(data[*idx].major, token);
+                token = strtok_s(NULL, "|", &context);
+            }
+            else
+            {
+                data[*idx].year = atoi(token);
+                token = NULL;
+            }
+        }
+    }
+    (*idx)++;
 
-	return;
+    return;
 }
 
-void printStudent(Student* data, int *idx)
+void printStudent(Student* data, int* idx)
 {
-	for (int i = 0; i < *idx; i++)
-	{
-		printf("%s ", data[i].name);
-		printf("%s ", data[i].id);
-		printf("%s ", data[i].major);
-		printf("%d ", data[i].year);
-	}
-	printf("\n");
+    printf("\n");
 
-	return;
+    for (int i = 0; i < *idx; i++)
+    {
+        printf("%s ", data[i].name);
+        printf("%s ", data[i].id);
+        printf("%s ", data[i].major);
+        printf("%d\n", data[i].year);
+    }
+
+    return;
 }
